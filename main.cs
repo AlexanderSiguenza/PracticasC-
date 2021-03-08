f1b9702f8d5637e01b255ad617633046467c9ab5
Alexander Siguenza <alexander.siguenza@udb.edu.sv>
2021-03-08 17:27:59 +0000

    Segundo ejemplo

--- main.cs
+++ main.cs
@@ -1,68 +1,35 @@
 using System;
 
+ // Programa que permite digitar un número entero positivo y me diga si es par o impar.
+ 
 class MainClass {
 
-  public static void Main (string[] args) {   
-   Console.ForegroundColor = ConsoleColor.Black; 
-   Console.BackgroundColor = ConsoleColor.White; 
-   
-   Console.Clear(); 
-   Console.Title = "Ejemplo1 de Funciones"; 
-   Double num1, num2, resultado; 
+  public static void Main (string[] args) {  
+    //Inicio del programa principal
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.Clear();
 
-   Console.WriteLine("Ingrese el primer número:"); 
-   num1=Double.Parse(Console.ReadLine()); 
-   
-   Console.WriteLine("Ingrese el segundo numero:"); 
-   num2=Double.Parse(Console.ReadLine());
-
-   Console.WriteLine("--->primero el procedimiento Suma"); 
-   Suma(num1, num2); // error en la suma
-
-   Console.WriteLine("\n"); 
-   Console.WriteLine("--->despues el procedimiento Multi"); 
-   Multi(num1, num2); 
-
-   Console.WriteLine("\n\n"); 
-   Console.WriteLine("--->tercera la función resta"); 
-   resultado = Resta(num1,num2);
-
-   Console.WriteLine("La resta es: " + resultado); 
-   Console.WriteLine("\n"); 
-   Console.WriteLine("-->Fin del programa"); 
-   Console.ReadKey();     
-   } 
-   
-   // procedimiento
-   static void Suma(Double pnum1, Double pnum2) 
-   { 
-    resultado = 0; 
-    resultado = pnum1 + pnum2; 
-    Console.WriteLine("La suma de los números es:" + resultado);     
-   } 
-   
-     // procedimiento
-   static void Multi(Double pnum1, Double pnum2) 
-   {      
-     resultado = 0;
-     resultado = pnum1 * pnum2; 
-     Console.WriteLine("La multiplicacion de los números es:" + resultado); 
-   } 
-      
-   // función   
-   static Double Resta(Double pnum1, Double pnum2) 
-    {  
-     resultado = 0;    
-     resultado = pnum1 - pnum2; 
-     return resultado;           
+    Console.Title = "Uso de un procedimiento";
+    int num;
+    Console.WriteLine("Digitar un numero entero positivo:");
+    num = int.Parse(Console.ReadLine());
+    Par(num);
+    Console.WriteLine("\n");
+    Console.WriteLine("-->Fin del programa");
+    Console.ReadKey();
   }
 
-   // función   
-   static Double Operaciones(Double pnum1, Double pnum2, Char ope) // + , * , - 
-    { 
-     Double resul; 
-     resul = pnum1 - pnum2; 
-     return resul;        
+  static void Par(int a)
+  {
+    int x;
+    x = (a % 2);
+    if (x == 0){
+      Console.WriteLine("\nEs par");
+    } else{
+      Console.WriteLine("\nEs impar");
+    }
   }
-  
+
+ 
 }
