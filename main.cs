1d70df0ba69d9fadfc6968a09e42ce473851a76a
Alexander Siguenza <alexander.siguenza@udb.edu.sv>
2021-03-08 17:24:37 +0000

    Realizar un programa que sume, multiplica y resta 2 números cualesquiera

--- main.cs
+++ main.cs
@@ -1,56 +1,68 @@
 using System;
 
-/* Programa que permite hacer conversión de monedas 
-   digitando una cantidad en dólares, utilizando 
-   funciones.*/
+class MainClass {
 
-class MainClass 
-{
-  public static void Main (string[] args) 
-  {
-    //Inicia el programa principal
-    Console.ForegroundColor = ConsoleColor.Black;
-    Console.BackgroundColor = ConsoleColor.White;
-    Console.Clear(); 
-    Console.Title = "Ejemplo3 utilizando la sintaxis de funciones";
-    Double cantidad, resul;
+  public static void Main (string[] args) {   
+   Console.ForegroundColor = ConsoleColor.Black; 
+   Console.BackgroundColor = ConsoleColor.White; 
+   
+   Console.Clear(); 
+   Console.Title = "Ejemplo1 de Funciones"; 
+   Double num1, num2, resultado; 
 
-    Console.WriteLine("Digitar la cantidad en dólares:");
-    cantidad = Double.Parse(Console.ReadLine()); 
+   Console.WriteLine("Ingrese el primer número:"); 
+   num1=Double.Parse(Console.ReadLine()); 
+   
+   Console.WriteLine("Ingrese el segundo numero:"); 
+   num2=Double.Parse(Console.ReadLine());
 
-    resul = conversiones(cantidad, 'e'); // euros
-    Console.WriteLine("Los {0} dolares son {1} euros ", cantidad, resul);
+   Console.WriteLine("--->primero el procedimiento Suma"); 
+   Suma(num1, num2); // error en la suma
 
-    resul = conversiones(cantidad, 'l'); // libras
-    Console.WriteLine("Los {0} dolares son {1} libras ", cantidad, resul); 
+   Console.WriteLine("\n"); 
+   Console.WriteLine("--->despues el procedimiento Multi"); 
+   Multi(num1, num2); 
 
-    resul = conversiones(cantidad, 'b'); // Bitcoin
-    Console.WriteLine("Los {0} dolares son {1} Bitcoin ", cantidad, resul); 
+   Console.WriteLine("\n\n"); 
+   Console.WriteLine("--->tercera la función resta"); 
+   resultado = Resta(num1,num2);
 
-    Console.WriteLine("\n");
-    Console.WriteLine("-->Fin del programa");
-    Console.ReadKey();
-  }  
+   Console.WriteLine("La resta es: " + resultado); 
+   Console.WriteLine("\n"); 
+   Console.WriteLine("-->Fin del programa"); 
+   Console.ReadKey();     
+   } 
+   
+   // procedimiento
+   static void Suma(Double pnum1, Double pnum2) 
+   { 
+    resultado = 0; 
+    resultado = pnum1 + pnum2; 
+    Console.WriteLine("La suma de los números es:" + resultado);     
+   } 
+   
+     // procedimiento
+   static void Multi(Double pnum1, Double pnum2) 
+   {      
+     resultado = 0;
+     resultado = pnum1 * pnum2; 
+     Console.WriteLine("La multiplicacion de los números es:" + resultado); 
+   } 
+      
+   // función   
+   static Double Resta(Double pnum1, Double pnum2) 
+    {  
+     resultado = 0;    
+     resultado = pnum1 - pnum2; 
+     return resultado;           
+  }
 
-  static Double conversiones(Double pcantidad , Char pmoneda)
-  { 
-    Double resul=0;
-    switch (pmoneda) 
-    {
-      case 'e': // euros
-        resul = pcantidad * 0.84;
-        break;
-      case 'l': // libras
-        resul = pcantidad * 0.72;
-        break;
-      case 'b': // Bitcoin
-        resul = pcantidad * 0.000020;
-        break;
-      default:
-        Console.WriteLine("Por favor ingresar valores validos /  (l)->libras o (e)->euros (b)->Bitcoin");
-        break;
-    }
-    return resul;
+   // función   
+   static Double Operaciones(Double pnum1, Double pnum2, Char ope) // + , * , - 
+    { 
+     Double resul; 
+     resul = pnum1 - pnum2; 
+     return resul;        
   }
   
 }
